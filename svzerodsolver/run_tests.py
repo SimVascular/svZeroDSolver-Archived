b36b817b1df24df22047946c6e7194fa302d3ad1
Jonathan Pham <39072822+JonathanPham@users.noreply.github.com>
2021-06-25 17:22:14 -0700

    Create new tests (#36)
    * deleted wall shear stress (wss, tau) calculations b/c they were not tested yet
    
    * deleted functions, extract_0d_cap_results, collapse_inlet_and_outlet_0d_results, and compute_time_averaged_result, b/c they were not tested/used
    
    * changed file name from svZeroDSolver.py to solver.py
    
    * updated comments
    
    * bug fix
    
    * moved run_tests.py from test_cases/ to svzerodsolver/ and added to pulsatile test cases to check that initialization from steady state soltn of simulations using mean flow BC works correctly
    
    * bug fix

--- svzerodsolver/run_tests.py
+++ svzerodsolver/run_tests.py
@@ -3,8 +3,8 @@
 
 import os
 import pdb
-import subprocess
 import numpy as np
+from . import solver
 
 def get_tests():
     """
@@ -73,6 +73,18 @@ def get_tests():
                                             Test('flow',     2,  0, -1,    2.5, 1.0e-8, 'point'),
                                             Test('flow',     2, -1, -1,    2.5, 1.0e-8, 'point')    ]
 
+    # check that initialization from steady state solution with mean flow work correctly
+    tests['pulsatileFlow_R_RCR']      = [   Test('pressure', 0,  0, 0,  4620.0, 1.0e-7, 'point'),
+                                            Test('pressure', 0, -1, 0,  4400.0, 1.0e-7, 'point'),
+                                            Test('flow',     0,  0, 0,     2.2, 1.0e-8, 'point'),
+                                            Test('flow',     0, -1, 0,     2.2, 1.0e-8, 'point')  ]
+
+    # check that initialization from steady state solution with mean flow work correctly
+    tests['pulsatileFlow_R_coronary'] = [   Test('pressure', 0,  0, 0, 880.0, 1.0e-7, 'point'),
+                                            Test('pressure', 0, -1, 0, 660.0, 1.0e-7, 'point'),
+                                            Test('flow',     0,  0, 0,   2.2, 1.0e-8, 'point'),
+                                            Test('flow',     0, -1, 0,   2.2, 1.0e-8, 'point')   ]
+
     return tests
 
 
@@ -96,11 +108,11 @@ class Test:
         # sanity checks
         if field not in ['flow', 'pressure']:
             raise ValueError('Field ' + field + ' unknown. Please select from flow, pressure')
-        if fun not in ['point', 'mean', 'max', 'min']:
+        if fun not in ['point', 'mean', 'max', 'min', 'time_average']:
             raise ValueError('Function ' + fun + ' unknown. Please select from point, mean, max, min')
         if not np.isscalar(time_step) and fun == 'point':
             raise ValueError('Specify a single time point when selecting result type ' + fun)
-        if np.isscalar(time_step) and (fun == 'mean' or fun == 'max' or fun == 'min'):
+        if np.isscalar(time_step) and (fun == 'mean' or fun == 'max' or fun == 'min' or fun == 'time_average'):
             raise ValueError('Specify a time interval when selecting result type ' + fun)
 
         self.field = field
@@ -132,13 +144,17 @@ class Test:
         Read results and select function
         """
         # extract result
-        res = results[self.field][self.branch][self.branch_node, self.time_step]
+        res  = results[self.field][self.branch][self.branch_node, self.time_step]
 
         # select result type
         if self.fun == 'point':
             return res
         elif self.fun == 'mean':
             return np.mean(res)
+        elif self.fun == 'time_average':
+            time = results["time"][self.time_step]
+            period = time[-1] - time[0]
+            return np.trapz(res, time)/period
         elif self.fun == 'max':
             return np.max(res)
         elif self.fun == 'min':
@@ -156,7 +172,7 @@ class Test:
         return err
 
 
-def read_results_0d(zero_d_simulation_results_file_path):
+def read_results_0d(zero_d_simulation_results_file_path, zero_d_simulation_ICs_file_path):
     """
     Read results from svZeroDSolver
     Args:
@@ -166,7 +182,7 @@ def read_results_0d(zero_d_simulation_results_file_path):
     """
     res = np.load(zero_d_simulation_results_file_path, allow_pickle = True).item()
     os.remove(zero_d_simulation_results_file_path) # remove branching structure results
-    os.remove(zero_d_simulation_results_file_path[:-18] + "all_results.npy") # remove all results
+    os.remove(zero_d_simulation_ICs_file_path)
     return res
 
 
@@ -185,26 +201,38 @@ def run_check(results, result_checks):
     return False
 
 
-def run_test(build_dir, test_dir, name, check):
+def run_test(test_dir, name, check):
     """
     Run a test case and check the results
     """
-    # name of svZeroDSolver executable
-    exe = os.path.join(build_dir, 'svZeroDSolver.py')
 
     # name of input file
     inp = os.path.join(test_dir, name + '.in')
 
     # run simulation
     try:
-        subprocess.check_output([exe, inp])
-    except subprocess.CalledProcessError as err:
-        return 'Test failed. svZeroDSolver returned error:\n' + err.output.decode("utf-8")
+        solver.set_up_and_run_0d_simulation(inp,
+                                            draw_directed_graph = False,
+                                            last_cycle = False,
+                                            save_results_all = False,
+                                            save_results_branch = True,
+                                            use_custom_0d_elements = False,
+                                            custom_0d_elements_arguments_file_path = None,
+                                            use_ICs_from_npy_file = False,
+                                            ICs_npy_file_path = None,
+                                            save_y_ydot_to_npy = False,
+                                            y_ydot_file_path = None,
+                                            check_jacobian = False,
+                                            simulation_start_time = 0.0,
+                                            use_steady_soltns_as_ics = True )
+    except Exception as err:
+        return 'Test failed. svZeroDSolver returned error:\n' + str(err)
 
     # extract results
     try:
         zero_d_simulation_results_file_path = inp[:-3] + "_branch_results.npy"
-        res = read_results_0d(zero_d_simulation_results_file_path)
+        zero_d_simulation_ICs_file_path     = inp[:-3] + "_initial_conditions.npy"
+        res = read_results_0d(zero_d_simulation_results_file_path, zero_d_simulation_ICs_file_path)
     except Exception as err:
         return 'Test failed. Result extraction failed:\n' + str(err)
 
@@ -222,16 +250,10 @@ def main():
     Loop over all test cases and check if all results match
     """
     # set paths
-    if 'BUILD_DIR' not in os.environ and 'TEST_DIR' not in os.environ:
+    if 'TEST_DIR' not in os.environ:
         # run locally
         fpath = os.path.dirname(os.path.realpath(__file__))
-        build_dir = os.path.join(fpath, '..')
-        test_dir = fpath
-    else:
-        # run on Travis
-        build_dir = os.environ['BUILD_DIR']
-        test_dir = os.environ['TEST_DIR']
-        # todo: need to use Travis to do automatic testing
+        test_dir = os.path.join(fpath, '..', 'test_cases')
 
     # get test cases
     try:
@@ -243,7 +265,7 @@ def main():
     # loop all test cases
     for name, check in test_cases.items():
         print('Running test ' + name)
-        err = run_test(build_dir, test_dir, name, check)
+        err = run_test(test_dir, name, check)
 
         # check if errors occured
         if err:
