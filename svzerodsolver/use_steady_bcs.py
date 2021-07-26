5559ea35543a4187f719a083bd253673b3375cbe
Jonathan Pham <jphamine@gmail.com>
2021-07-26 16:10:42 -0700

    allow for non-periodic coronary BCs

--- svzerodsolver/use_steady_bcs.py
+++ svzerodsolver/use_steady_bcs.py
@@ -110,9 +110,6 @@ def convert_unsteady_bcs_to_steady(parameters):
                 if len(time) < 2:
                         message = "Error. Boundary condition, " + bc_name + ", must be prescribed for at least 2 time points."
                         raise RuntimeError(message)
-                if bc_values[0] != bc_values[-1]:
-                    message = "Error. Boundary condition, " + bc_name + ", is not periodic. The two endpoints for the time series must be the same."
-                    raise RuntimeError(message)
                 cardiac_cycle_period = time[-1] - time[0]
                 time_averaged_value = compute_time_averaged_bc_value_for_single_cardiac_cycle(time, bc_values, cardiac_cycle_period)
                 parameters["boundary_conditions"][index_of_bc]["bc_values"]["t"]           = [time[0], time[-1]]
